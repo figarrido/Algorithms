d7bda0893d17e0302c8b7c00171661b2855cd7ef
figarrido <fgarridob.95@gmail.com>
2015-08-04 13:29:44 -0300

    Update GraphU

--- GraphUndirected.py
+++ GraphUndirected.py
@@ -10,10 +10,10 @@ primer dato corresponde a la etiqueta del vértice y los siguientes son las
 etiquetas de los vértices adjacentes.
 
 Ejemplo de archivo:
-1	2	3
-2	1	3	4
-3	1	2	4
-4	2	3
+1   2   3
+2   1   3   4
+3   1   2   4
+4   2   3
 
 
 cargarGrafo retorna:
@@ -38,8 +38,16 @@ class Vertex:
         if self not in Vertex.V:
             Vertex.V += [self]
 
-    def add_edge(self, edge):
+    def addEdge(self, edge):
         self.adjacent_list += [edge]
+
+    @property
+    def edges(self):
+        return self.adjacent_list
+
+    @edges.setter
+    def edges(self, other):
+        self.adjacent_list = other
 
     def __eq__(self, other):
         return self.label == other.label
@@ -47,21 +55,56 @@ class Vertex:
     def __str__(self):
         return str(self.label)
 
+    @staticmethod
+    def index(value):
+        for i in range(len(Vertex.V)):
+            if Vertex.V[i].label == value:
+                return i
+        return -1
+
 
 class Edge:
     E = []
 
     def __init__(self, u, v):
-        self.v = v
         self.u = u
+        self.v = v
 
         if self not in Edge.E:
             Edge.E += [self]
 
-    def contain(self, vertex):
-        if self.u == vertex or self.v == vertex:
-            return True
-        return False
+    def _contain(self, vertex):
+        return self.u == vertex or self.v == vertex
+
+    def contain(self, vertex1, vertex2=None):
+        if not vertex2:
+            return self._contain(vertex1)
+        else:
+            return self._contain(vertex1) and self._contain(vertex2)
+
+    @property
+    def vertexA(self):
+        return self.u
+
+    @vertexA.setter
+    def vertexA(self, other):
+        self.u = other
+
+    @property
+    def vertexB(self):
+        return self.v
+
+    @vertexB.setter
+    def vertexB(self, other):
+        self.v = other
+
+    @property
+    def vertices(self):
+        return self.u, self.v
+
+    @property
+    def isLoop(self):
+        return self.u == self.v
 
     def __eq__(self, other):
         if self.u == other.u and self.v == other.v:
@@ -77,18 +120,30 @@ class Edge:
 def cargarGrafo(filename, sep='\t'):
     with open(filename, "r") as file:
         info = file.readlines()
-
     info = [i.strip() for i in info]
-
-    for v in info:
-        data = v.split(sep)
+    aristas = []
+    for line in info:
+        data = line.split(sep)
         label = int(data[0])
         u = Vertex(label)
-        [Edge(u, Vertex(int(i))) for i in data[1:]]
+        for i in data[1:]:
+            if (label, int(i)) not in aristas\
+                    and (int(i), label) not in aristas:
+                aristas += [(label, int(i))]
 
-    for v in Vertex.V:
-        for e in Edge.E:
-            if e.contain(v):
-                v.add_edge(e)
+    for a in aristas:
+        for j in Vertex.V:
+            if j.label == a[0]:
+                u = j
+            if j.label == a[1]:
+                v = j
+        e = Edge(u, v)
+        u.addEdge(e)
+        v.addEdge(e)
 
     return Vertex.V, Edge.E
+
+
+def limpiar():
+    Vertex.V.clear()
+    Edge.E.clear()
